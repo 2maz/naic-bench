473d643800351c91e62168eda3c4e0af8787c588
Thomas Roehr <roehr@simula.no>
2026-01-30 23:36:53 +0100

    test: add test_metrics

--- tests/naic_bench/test_spec.py
+++ tests/naic_bench/test_spec.py
@@ -1,5 +1,6 @@
 import pytest
 from naic_bench.spec import BenchmarkSpec
+from naic_bench.utils import find_confd
 
 def test_load(testdir, tmp_path):
     specfile = testdir / "data" / "conf.d" / "a.yaml"
@@ -8,3 +9,15 @@ def test_load(testdir, tmp_path):
     bc = specs["pytorch"]["a"]["fp16"]
     bc.expand_placeholders(CPU_COUNT=128)
     assert bc.arguments["train-loader-workers"] == '32'
+
+@pytest.mark.parametrize("name,metric,expected,teststring",
+    [
+        ["transformerxl_base","throughput", 140123.0, "Training throughput: 140123 Tok/s"]
+    ]
+)
+def test_metrics(name, metric, expected, teststring, tmp_path):
+    benchmarks = BenchmarkSpec.load_all(confd_dir=find_confd(), data_dir=tmp_path)
+    config = benchmarks["pytorch"][name]
+
+    for variant, spec in config.items():
+        assert spec.extract_metrics([teststring])[metric] == expected
