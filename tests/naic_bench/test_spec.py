c5b289baae8362f80731084e0f1c3ca29155156f
Thomas Roehr <roehr@simula.no>
2026-02-09 16:13:35 +0100

    Fix ssd setup

--- tests/naic_bench/test_spec.py
+++ tests/naic_bench/test_spec.py
@@ -12,7 +12,23 @@ def test_load(testdir, tmp_path):
 
 @pytest.mark.parametrize("name,metric,expected,teststring",
     [
-        ["transformerxl_base","throughput", 140123.0, "Training throughput: 140123 Tok/s"]
+        ["transformerxl_base","throughput", 140123.0, [
+                "Training throughput: 140123 Tok/s"
+            ]
+        ],
+        ["ssd", "throughput", 646.242, [
+                "Creating index...",
+                "Done (t=0.48s)",
+                "DLL 2026-02-09 15:50:00.948517 - () avg_img/sec : 646.2415546927269 images/s med_img/sec : 646.3537480022912 images/s min_img/sec : 642.838741874282 images/s max_img/sec : 649.0996051242621 images/s",
+                "Done benchmarking. Total images: 57000  total time: 88.202      Average images/sec: 646.242     Median images/sec: 646.354",
+                "Training performance = 646.353759765625 FPS",
+                "DLL 2026-02-09 15:50:01.338979 - (0,) time : 159.1902894973755",
+                "saving model...",
+                "DLL 2026-02-09 15:50:01.454263 - (0, 0) model path : /tmp/ssd_fp32/models/epoch_0.pt",
+                "DLL 2026-02-09 15:50:01.454558 - () total time : 159.1902894973755",
+                "DLL 2026-02-09 15:50:01.454575 - ()",
+            ]
+        ]
     ]
 )
 def test_metrics(name, metric, expected, teststring, tmp_path):
@@ -20,4 +36,4 @@ def test_metrics(name, metric, expected, teststring, tmp_path):
     config = benchmarks["pytorch"][name]
 
     for variant, spec in config.items():
-        assert spec.extract_metrics([teststring])[metric] == expected
+        assert spec.extract_metrics(teststring)[metric] == expected
